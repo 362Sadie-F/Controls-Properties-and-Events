d0d785a79ebbaf97858fb5defc0a174c24dd8849
362Sadie-F <princessmae.fleming@gmail.com>
2026-03-30 12:26:59 -0400

    Add project files.

---
+++ Controls, Properties and Events/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Controls__Properties_and_Events
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.imgCharacter = new System.Windows.Forms.PictureBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgCharacter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgCharacter
+            // 
+            this.imgCharacter.Image = ((System.Drawing.Image)(resources.GetObject("imgCharacter.Image")));
+            this.imgCharacter.Location = new System.Drawing.Point(28, 107);
+            this.imgCharacter.Name = "imgCharacter";
+            this.imgCharacter.Size = new System.Drawing.Size(284, 285);
+            this.imgCharacter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgCharacter.TabIndex = 0;
+            this.imgCharacter.TabStop = false;
+            this.imgCharacter.Visible = false;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnSubmit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSubmit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSubmit.Font = new System.Drawing.Font("Kristen ITC", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(352, 225);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(131, 39);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Click Me";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblInstruction.Font = new System.Drawing.Font("Old English Text MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstruction.Location = new System.Drawing.Point(247, 24);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(598, 44);
+            this.lblInstruction.TabIndex = 2;
+            this.lblInstruction.Text = "Click on the Button or the Picture Box";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblStatus.Font = new System.Drawing.Font("Old English Text MT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(455, 89);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(106, 32);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Status: ";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 482);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblInstruction);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.imgCharacter);
+            this.Name = "MainForm";
+            this.Text = "Controls, Events and Properties";
+            ((System.ComponentModel.ISupportInitialize)(this.imgCharacter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgCharacter;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Controls, Properties and Events/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Controls__Properties_and_Events
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            imgCharacter.Visible = true;
+            lblStatus.Text = "Status: Clicked";
+        }
+    }
+}
